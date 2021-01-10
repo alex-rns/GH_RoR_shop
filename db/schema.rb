fa0bc57b92b30761112ab746c393924a410a06c0
Oleksandr Bielodubrovskyi <alexrns3@gmail.com>
2021-01-10 21:07:47 +0200

    Added resourse Category, controller Pages. Created header and sidebar.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_10_144043) do
+ActiveRecord::Schema.define(version: 2021_01_10_163252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -22,6 +29,9 @@ ActiveRecord::Schema.define(version: 2021_01_10_144043) do
     t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
+  add_foreign_key "products", "categories"
 end
