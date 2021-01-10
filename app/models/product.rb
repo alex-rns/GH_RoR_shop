e67c22e303f46979feb0d031bcee4c7867101b89
Oleksandr Bielodubrovskyi <alexrns3@gmail.com>
2021-01-10 23:04:49 +0200

    Changed home layouts(header/sidebar).Changed type on decimal in the column price. Set up anotate in model.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :bigint           not null, primary key
+#  name        :string
+#  description :text
+#  price       :decimal(10, 2)
+#  image       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  category_id :bigint
+#
 class Product < ApplicationRecord
   belongs_to :category
   validates :name, :description, :price, presence: true
---
+++ db/migrate/20210110194915_change_price_column_type_to_decimal.rb
@@ -0,0 +1,5 @@
+class ChangePriceColumnTypeToDecimal < ActiveRecord::Migration[6.1]
+  def change
+    change_column(:products, :price, :decimal)
+  end
+end
