c58b6bb188ea2bdb570f0e2a420ee09b10e01a84
Oleksandr Bielodubrovskyi <alexrns3@gmail.com>
2021-01-19 21:14:38 +0200

    Added check for existing product in the cart when AddToCard, if product exist - quantity increased. Added delete button product from cart

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,7 +1,13 @@
 class OrderItemsController < ApplicationController
   def create
-    order_item = current_order.order_items.create(product_id: params[:product_id])
-    redirect_to order_path(current_order)
+    order_item = current_order.order_items.find_by(product_id: params[:product_id])
+    if order_item.present?
+      order_item.update(quantity: order_item.quantity + 1)
+      redirect_to order_path(current_order)
+    else
+      current_order.order_items.create(product_id: params[:product_id])
+      redirect_to order_path(current_order)
+    end
   end
 
   def update
@@ -11,5 +17,8 @@ class OrderItemsController < ApplicationController
   end
 
   def destroy
+    order_item = OrderItem.find(params[:id])
+    order_item.destroy
+    redirect_to order_path(current_order)
   end
 end
