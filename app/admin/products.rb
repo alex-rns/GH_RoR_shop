5bb366ccb3276d97ad396a35c4f8c1c939365460
Oleksandr Bielodubrovskyi <alexrns3@gmail.com>
2021-01-21 23:19:36 +0200

    Added permit

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register Product do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :name, :description, :price, :image, :category_id
+  permit_params :name, :description, :price, :image, :category_id
   #
   # or
   #
