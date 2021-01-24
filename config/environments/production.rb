13ad5d14771381871b31c3aa03ea29ab9b51189f
Oleksandr Bielodubrovskyi <alexrns3@gmail.com>
2021-01-24 13:06:07 +0200

    Checking mailer on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,6 +40,20 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :amazon
 
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'ror-shop.herokuapp.com' }
+
+  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            'alexrnsweb@gmail.com',
+    password:             'alexrnsWeb@2',
+    authentication:       'plain',
+    enable_starttls_auto: true }
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
