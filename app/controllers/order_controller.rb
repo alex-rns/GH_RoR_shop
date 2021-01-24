b72e8054cce11f10801e9092cbf0d44d3830e277
Oleksandr Bielodubrovskyi <alexrns3@gmail.com>
2021-01-24 14:32:57 +0200

    After confirmation the order status becomes "ordered"

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -6,6 +6,7 @@ class OrderController < ApplicationController
 
   def complete
     OrderMailer.with(user: current_user, order: current_order).complete_order.deliver_now
+    current_order.update({status: 1})
   end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ApplicationRecord
   belongs_to :user
-  has_many :order_items
+  has_many :order_items, dependent: :destroy
   enum status: [:in_progress, :ordered, :canceled]
 end
