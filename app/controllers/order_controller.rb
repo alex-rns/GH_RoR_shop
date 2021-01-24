d0d0bb5cc4a54ff0bf7d7175649809d9f1345040
Oleksandr Bielodubrovskyi <alexrns3@gmail.com>
2021-01-24 15:36:19 +0200

    Added the ability for the user to make more than one order

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -7,6 +7,7 @@ class OrderController < ApplicationController
   def complete
     OrderMailer.with(user: current_user, order: current_order).complete_order.deliver_now
     current_order.update({status: 1})
+    Order.create(user: current_user)
   end
 
 end
