f4d36a1c48e2a486ee1d55d979740e6c8a63ee20
Oleksandr Bielodubrovskyi <alexrns3@gmail.com>
2021-01-27 00:56:10 +0200

    Try heroku fix

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -1,5 +1,5 @@
 class OrderController < ApplicationController
-  before_action :authenticate_user!, raise: false
+  # before_action :authenticate_user!, raise: false
   def show
     @items = current_order.order_items.order("id DESC")
   end
