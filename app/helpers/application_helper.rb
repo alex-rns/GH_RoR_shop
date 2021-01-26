228e86431fdd09abd084ad75e73dde5dd7cdb2d6
Oleksandr Bielodubrovskyi <alexrns3@gmail.com>
2021-01-26 22:20:11 +0200

    Added helper when user leaves a comment only on purchased product

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def user_purchased_product?(product)
+    OrderItem.where(product_id: product.id).any? do |item|
+      item if item.order.user_id == current_user.id
+    end
+  end
+
 end
