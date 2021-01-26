b61eb2269e6380aaa2b13ce2e0df1d1ae98260d2
Oleksandr Bielodubrovskyi <alexrns3@gmail.com>
2021-01-27 00:53:44 +0200

    Heroku try fix error "ArgumentError: Before process_action callback :authenticate_user! has not been defined"

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -1,5 +1,5 @@
 class OrderController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, raise: false
   def show
     @items = current_order.order_items.order("id DESC")
   end
