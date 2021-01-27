2fc173e3667fd756add44c329bdbf91761f58251
Oleksandr Bielodubrovskyi <alexrns3@gmail.com>
2021-01-27 22:30:14 +0200

    Styled sign in/register forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:github]
+         :rememberable, :validatable, :omniauthable, omniauth_providers: [:github]
 
   has_many :order_items
   has_many :orders
