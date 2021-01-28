c0ba1b9aa5d0b62f6f29ef764d9d9494ea1de208
Oleksandr Bielodubrovskyi <alexrns3@gmail.com>
2021-01-28 17:24:45 +0200

    Fixed work will_paginate

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,22 +1,20 @@
 class CategoriesController < ApplicationController
-  WillPaginate.per_page = 8
 
   def show
-
     @category = Category.find(params[:id])
     @categories = @category.subcategories
     products_all_category = @category.subcategories.map(&:id) << @category.id
     # sorting product
     @products = if params[:sort] == "cheapest"
-                  Product.where(category_id: products_all_category).order(price: :ASC).page(params[:page])
+                  Product.where(category_id: products_all_category).order(price: :ASC).paginate(page: params[:page], per_page: 8)
                 elsif params[:sort] == "expensive"
-                  Product.where(category_id: products_all_category).order(price: :DESC).page(params[:page])
+                  Product.where(category_id: products_all_category).order(price: :DESC).paginate(page: params[:page], per_page: 8)
                 elsif params[:sort] == "alphabetically"
-                  Product.where(category_id: products_all_category).order(name: :ASC).page(params[:page])
+                  Product.where(category_id: products_all_category).order(name: :ASC).paginate(page: params[:page], per_page: 8)
                 elsif params[:min_price].present? || params[:max_price].present?
-                  Product.where(category_id: products_all_category).where(price: [params[:min_price]].first..[params[:max_price]].last).page(params[:page])
+                  Product.where(category_id: products_all_category).where(price: [params[:min_price]].first..[params[:max_price]].last).paginate(page: params[:page], per_page: 8)
                 else
-                  Product.where(category_id: products_all_category).order(created_at: :ASC).page(params[:page])
+                  Product.where(category_id: products_all_category).order(created_at: :ASC).paginate(page: params[:page], per_page: 8)
                 end
     render "products/index"
   end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   def index
     # searching with autocomplete
-    @products = Product.order(:name).where("name ilike ?", "%#{params[:term]}%").page(params[:page])
+    @products = Product.order(:name).where("name ilike ?", "%#{params[:term]}%").paginate(page: params[:page], per_page: 8)
     respond_to do |format|
       format.html
       format.json { render json: @products.map(&:name) }
@@ -9,15 +9,15 @@ class ProductsController < ApplicationController
 
     # sorting product
     @products = if params[:sort] == "cheapest"
-                  Product.order(price: :ASC).page(params[:page])
+                  Product.order(price: :ASC).paginate(page: params[:page], per_page: 8)
                 elsif params[:sort] == "expensive"
-                  Product.order(price: :DESC).page(params[:page])
+                  Product.order(price: :DESC).paginate(page: params[:page], per_page: 8)
                 elsif params[:sort] == "alphabetically"
-                  Product.order(name: :ASC).page(params[:page])
+                  Product.order(name: :ASC).paginate(page: params[:page], per_page: 8)
                 elsif params[:min_price].present? || params[:max_price].present?
-                  Product.where(price: [params[:min_price]].first..[params[:max_price]].last).page(params[:page])
+                  Product.where(price: [params[:min_price]].first..[params[:max_price]].last).paginate(page: params[:page], per_page: 8)
                 else
-                  Product.all.order(created_at: :ASC).page(params[:page])
+                  Product.all.order(created_at: :ASC).paginate(page: params[:page], per_page: 8)
                 end
     @categories = Category.where(parent_id: nil)
   end
@@ -31,9 +31,9 @@ class ProductsController < ApplicationController
   def search
     if @category.present?
       @category = Category.find(params[:category_id])
-      @products = Product.where(category_id: params[:category_id]).where("name ILIKE ? OR description ILIKE ?", "%#{params[:query]}%", "%#{params[:query]}%").page(params[:page])
+      @products = Product.where(category_id: params[:category_id]).where("name ILIKE ? OR description ILIKE ?", "%#{params[:query]}%", "%#{params[:query]}%").paginate(page: params[:page], per_page: 8)
     end
-    @products = Product.where("name ILIKE ? OR description ILIKE ?", "%#{params[:query]}%", "%#{params[:query]}%").page(params[:page])
+    @products = Product.where("name ILIKE ? OR description ILIKE ?", "%#{params[:query]}%", "%#{params[:query]}%").paginate(page: params[:page], per_page: 8)
     render "products/index"
   end
 end
