7f0e0a2892fea91e664cb19b4a554d7c7ae27653
Oleksandr Bielodubrovskyi <alexrns3@gmail.com>
2021-02-03 20:52:34 +0200

    Set domen

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,5 +6,5 @@ set :deploy_to, "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :environment, "production"
 set :rails_env,   "production"
 
-set :nginx_server_name, "104.248.133.233"
+set :nginx_server_name, "drum-shop.sytes.net"
 set :puma_conf, "#{shared_path}/config/puma.rb"
