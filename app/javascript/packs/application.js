15759f743be1f73d0294bc1af80907cf539718f7
Oleksandr Bielodubrovskyi <alexrns3@gmail.com>
2021-02-08 17:47:06 +0200

    Added jQ plugin that adds a magnifying glass style zoom functionality to images

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,9 @@ import Turbolinks from "turbolinks"
 // import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
-
+import "magnify"
 import './products.js'
+import './zoom.js'
 
 document.addEventListener("turbolinks:load", function() {
     $(function () {
